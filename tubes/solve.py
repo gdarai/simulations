f5f0980a097b23dbd3a9be27510e9bc28b060d8c
tr949 <martin.holecek@deutsche-boerse.com>
2022-07-27 11:58:14 +0200

    next part

--- tubes/solve.py
+++ tubes/solve.py
@@ -12,14 +12,14 @@ import csv
 SETTING = 'setting.json'
 EMPTY = ' '
 EMPTY_NUM = 0
-debug = 2
+debug = 1
 state = dict();
 
 ########
 # Functions
 
 def printDebug(lvl, data):
-	if(debug > lvl): print(data)
+	if(debug >= lvl): print(data)
 
 # calc 1 tube size
 def calcSize(tubes):
@@ -37,6 +37,7 @@ def getColors(colors):
 	colMap[EMPTY] = EMPTY_NUM
 	for idx, key in enumerate(keys):
 		colMap[key] = idx + 1
+
 	return colMap;
 
 # init the state string
@@ -50,6 +51,18 @@ def stringStateFromInit(tubes):
 	printDebug(2, out)
 	return out
 
+# get current state string
+def stringState():
+	size = state['size']
+	colMap = state['colorsInv']
+	out = ''
+	for tube in state['tubes']:
+		for fill in tube[1]:
+			out = out + colMap[fill[0]]*fill[1]
+		out = out + EMPTY*tube[0]
+	printDebug(2, out)
+	return out
+
 def setArrStateLambda(liq):
 	return [state['colors'][liq[0]],len(liq)]
 
@@ -59,7 +72,15 @@ def setArrState(stringState):
 	strTubes =[stringState[y-x:y] for y in range(x, len(stringState)+x,x)]
 	for idx, tube in enumerate(strTubes):
 		tubeSplit = [m.group(0) for m in re.finditer(r"(.)\1*", tube)]
-		state['tubes'][idx] = list(map(setArrStateLambda, tubeSplit));
+		tubeSplitMap = list(map(setArrStateLambda, tubeSplit));
+		tubeFree = x
+		if(len(tubeSplitMap) > 0):
+			if(tubeSplitMap[-1][0] == 0):
+				tubeFree = tubeSplitMap[-1][1]
+				tubeSplitMap = tubeSplitMap[0:-1]
+			else:
+				tubeFree = 0
+		state['tubes'][idx] = [tubeFree,tubeSplitMap]
 		printDebug(3, state)
 
 # cinit the state object
@@ -69,6 +90,7 @@ def initState(source):
 		state['size'] = calcSize(source['tubes'])
 
 	state['colors'] = getColors(source['colors'])
+	state['colorsInv'] = {v: k for k, v in state['colors'].items()}
 	state['tubes'] = [None] * len(source['tubes'])
 	initState = stringStateFromInit(source['tubes'])
 	state['toCheck'] = [[initState, '']]
@@ -89,6 +111,30 @@ def checkState():
 def randomChangeMap():
 	random.shuffle(state['from'])
 	random.shuffle(state['to'])
+
+# tweak the state by removing a content
+def tweakStateRemove(idx):
+	tube = state['tubes'][idx]
+	if(tube[0] == state['size']): return False
+	out = tube[1][-1]
+	tube[1] = tube[1][0:-1]
+	tube[0] = tube[0] + out[1]
+	return out
+
+# tweak the state by adding a content
+def tweakStateAdd(toAdd, idx):
+	tube = state['tubes'][idx]
+	if(tube[0] < toAdd[1]): return False
+	tube[0] = tube[0] - toAdd[1]
+	if(len(tube[1]) == 0):
+		tube[1] = toAdd
+		return True
+	lastFill = tube[1][-1]
+	if(lastFill[0] == toAdd[0]):
+		lastFill[1] = lastFill[1] + toAdd[1]
+		return True
+	tube[1].append(toAdd)
+	return True
 
 ########
 # Settings file
@@ -112,15 +158,18 @@ randomChangeMap()
 
 for i in state['from']:
 	iOrig = copy.deepcopy(state['tubes'][i])
+	toAdd = tweakStateRemove(i)
+	if(toAdd == False): continue
 	for j in state['to']:
 		if(i == j): continue
 		jOrig = copy.deepcopy(state['tubes'][j])
-		if(tweakArrState(i, j) == True):
-			if(checkState()): #WIN
-				print('Winner')
-				quit()
-			print('NotWinner')
-			#ADD to toCheck
+		if(tweakStateAdd(toAdd, j) == False): continue
+		if(checkState()): #WIN
+			print('Winner')
+			quit()
+		print('NotWinner')
+		stringState()
+		#ADD to toCheck
 		state['tubes'][j] = jOrig
 	state['tubes'][i] = iOrig
 
